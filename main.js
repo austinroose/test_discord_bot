2e062ad59c0b16e790db18424c62021b0e04410b
austinroose <austin.roose1@gmail.com>
2021-02-10 23:38:21 +0200

    discord bot

--- main.js
+++ main.js
@@ -7,11 +7,28 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
 
-client.on('message', msg => {
-    console.log(msg.content)
-    if (msg.content === "ping") {
-        msg.reply("pong")
+client.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    const is_admin = message.member.roles.highest.name === 'adminid'
+    if (is_admin) {
+        if (!message.guild) return;
+
+        if (message.content === '/leave') {
+            if (message.member.voice.channel) {
+                const disconnection = await message.member.voice.channel.leave();
+            }
+        }
+    
+        if (message.content === '/join') {
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+        }
     }
-}); 
+  });
 
 client.login(process.env.DISCORD_TOKEN)
